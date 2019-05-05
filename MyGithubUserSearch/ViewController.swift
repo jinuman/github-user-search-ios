f5930e0c0164746cc0baf8fbfff00a87317e3860
jinuman <jin35kim@gmail.com>
2019-05-05 14:36:07 +0900

    Start Github user search project

---
+++ MyGithubUserSearch/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyGithubUserSearch
+//
+//  Created by Jinwoo Kim on 05/05/2019.
+//  Copyright © 2019 jinuman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
