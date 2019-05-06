e09f3d084de0140cdd5d55221ce6fc218bf6e0a4
jinuman <jin35kim@gmail.com>
2019-05-06 17:56:59 +0900

    style: Set group structures

---
+++ MyGithubUserSearch/Sources/UserSearch/UserSearchCell.swift
@@ -0,0 +1,21 @@
+//
+//  UserSearchCell.swift
+//  MyGithubUserSearch
+//
+//  Created by Jinwoo Kim on 06/05/2019.
+//  Copyright © 2019 jinuman. All rights reserved.
+//
+
+import UIKit
+
+class UserSearchCell: UICollectionViewCell {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
