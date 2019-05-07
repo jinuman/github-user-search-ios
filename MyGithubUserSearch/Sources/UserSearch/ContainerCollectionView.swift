b802bd5479e5b272796350be10a35854e03a689b
jinuman <jin35kim@gmail.com>
2019-05-07 21:00:10 +0900

    feat: Add dummy collectionView and subviews

--- MyGithubUserSearch/Sources/UserSearch/ContainerCollectionView.swift
+++ MyGithubUserSearch/Sources/UserSearch/ContainerCollectionView.swift
@@ -6,4 +6,18 @@
 //  Copyright © 2019 jinuman. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class ContainerCollectionView: UICollectionView {
+    override init(frame: CGRect, collectionViewLayout layout: UICollectionViewLayout) {
+        let flowLayout = UICollectionViewFlowLayout()
+        flowLayout.scrollDirection = .horizontal
+        
+        super.init(frame: frame, collectionViewLayout: flowLayout)
+        register(Reusable.organizationCell)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
--- MyGithubUserSearch/Sources/UserSearch/OrganizationCell.swift
+++ MyGithubUserSearch/Sources/UserSearch/OrganizationCell.swift
@@ -6,4 +6,36 @@
 //  Copyright © 2019 jinuman. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class OrganizationCell: UICollectionViewCell {
+    var org: Organization? {
+        didSet {
+            guard let urlString = org?.avatarUrl else { return }
+            self.orgImageView.loadImageUsingCache(with: urlString)
+        }
+    }
+    
+    let orgImageView = UIImageView().then {
+        $0.contentMode = .scaleAspectFill
+        $0.clipsToBounds = true
+        $0.layer.cornerRadius = Metric.orgImageSize / 2
+        $0.layer.borderWidth = Metric.orgBorderWidth
+        $0.layer.borderColor = UIColor.lightGray.cgColor
+        
+        $0.backgroundColor = .yellow
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        addSubview(orgImageView)
+        
+        orgImageView.centerInSuperview()
+        orgImageView.constrainWidth(constant: Metric.orgImageSize)
+        orgImageView.constrainHeight(constant: Metric.orgImageSize)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
