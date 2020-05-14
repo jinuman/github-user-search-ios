0cf7543c9e08c0921ecb984a4af319cf8a9bbca6
Jinwoo Kim <jin35kim@gmail.com>
2020-05-14 11:50:41 +0900

    feat: Add UITableView and UICollectionView methods

--- MyGithubUserSearch/Helpers/UIKitExtensions.swift
+++ MyGithubUserSearch/Helpers/UIKitExtensions.swift
@@ -31,3 +31,122 @@ extension UIView {
         }
     }
 }
+
+extension UITableView {
+    
+    func setContentInsetWithScrollIndicatorsInset(contentInset: UIEdgeInsets) {
+        self.contentInset = contentInset
+        self.scrollIndicatorInsets = contentInset
+    }
+    
+    public func registerNibName(_ name: String) {
+        register(UINib(nibName: name, bundle: nil), forCellReuseIdentifier: name)
+    }
+    
+    public func dequeueReusableCell<T: UITableViewCell>(cellType: T.Type, for indexPath: IndexPath) -> T {
+        return dequeueReusableCell(withIdentifier: "\(T.self)", for: indexPath) as? T ?? T()
+    }
+    
+    /**
+     다수의 UITableViewCell들을 동시에 register 할 수 있도록 도와준다.
+     
+     - Parameters:
+        - cellTypes: 다수의 UITableViewCell.Type들로 이루어진 Array
+     */
+    func register(_ cellTypes: [UITableViewCell.Type]) {
+        for cellType in cellTypes {
+            self.register(cellType, forCellReuseIdentifier: "\(cellType.self)")
+        }
+    }
+    
+    // MARK: - Useful
+    
+    func hasRowAtIndexPath(indexPath: IndexPath) -> Bool {
+        return self.numberOfSections > indexPath.section
+            && self.numberOfRows(inSection: indexPath.section) > indexPath.row
+    }
+    
+    func selectRowSafely(
+        at indexPath: IndexPath,
+        animated: Bool,
+        scrollPosition: UITableView.ScrollPosition)
+    {
+        guard self.hasRowAtIndexPath(indexPath: indexPath) else { return }
+        self.selectRow(
+            at: indexPath,
+            animated: animated,
+            scrollPosition: scrollPosition
+        )
+    }
+    
+    func scrollToTopRowSafely(
+        at scrollPosition: UITableView.ScrollPosition = .top,
+        animated: Bool = true)
+    {
+        guard self.numberOfSections > 0,
+            self.numberOfRows(inSection: 0) > 0 else {
+                return
+        }
+        
+        self.scrollToRow(
+            at: IndexPath(row: 0, section: 0),
+            at: scrollPosition,
+            animated: animated
+        )
+    }
+    
+    func scrollToRowSafely(
+        at indexPath: IndexPath,
+        at scrollPosition: UITableView.ScrollPosition,
+        animated: Bool)
+    {
+        guard indexPath.section >= 0,
+            self.numberOfSections > indexPath.section else {
+                self.scrollToTop()
+                return
+        }
+        
+        guard indexPath.row >= 0,
+            self.numberOfRows(inSection: indexPath.section) > indexPath.row else {
+                self.scrollToTop()
+                return
+        }
+        
+        self.scrollToRow(
+            at: indexPath,
+            at: scrollPosition,
+            animated: animated
+        )
+    }
+    
+    func clearSeparatorWhenEmpty() {
+        self.tableFooterView = UIView()
+    }
+}
+
+extension UICollectionView {
+    
+    func registerNibName(_ name: String) {
+        register(UINib(nibName: name, bundle: nil), forCellWithReuseIdentifier: name)
+    }
+    
+    func dequeueReusableCell<T: UICollectionViewCell>(
+        cellType: T.Type,
+        for indexPath: IndexPath)
+        -> T
+    {
+        return dequeueReusableCell(withReuseIdentifier: "\(T.self)", for: indexPath) as? T ?? T()
+    }
+    
+    /**
+     다수의 UICollectionViewCell들을 동시에 register 할 수 있도록 도와준다.
+     
+     - Parameters:
+     - cellTypes: 다수의 UICollectionViewCell.Type들로 이루어진 Array
+     */
+    func register(_ cellTypes: [UICollectionViewCell.Type]) {
+        for cellType in cellTypes {
+            self.register(cellType, forCellWithReuseIdentifier: "\(cellType.self)")
+        }
+    }
+}
